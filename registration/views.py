b719bf17bd23754afd21a0df0b8ed3529e381563
sindhuakula <saiakulasindhu@gmail.com>
2016-06-08 16:12:38 +0530

    aums_id instead stud_id

--- registration/views.py
+++ registration/views.py
@@ -68,6 +68,7 @@ class StudentRegistrationView( LoginRequiredMixin, FormView):
 
     def form_valid(self, form):
         form.save()
+        form.instance.aums_id = form.instance.aums_id.lower()
         return FormView.form_valid(self, form)
 
 class StudentBulkUploadView( LoginRequiredMixin, FormView):
@@ -105,12 +106,12 @@ class StudentListView(LoginRequiredMixin,ListView):
 
 class StudentListUpdateView(LoginRequiredMixin, UpdateView):
     model = Student
-    form_class = StudentRegistrationForm
+    fields = student_fields
     template_name_suffix = '_update_form'
     success_url = '/register/cirstaff/success/'
 
     def get_object(self, queryset=None):
-        obj = Student.Objects.get(stud_id=self.kwargs['stud_id'])
+        obj = Student.Objects.get(aums_id=self.kwargs['aums_id'])
         if obj:
             return obj
         else:
